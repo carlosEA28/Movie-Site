8bee02cb5ffc452f7d4fca1eeb6192f29820f28a
carlosEA28 <carloseaduarte28@gmail.com>
2025-01-16 10:27:45 -0300

    first commit creating movieCard component

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>opi</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,18 @@
+function MovieCard({ movie }) {
+  function onFavoriteClick() {
+    alert("Clicekd");
+  }
+
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img src={movie.url} alt={movie.title} />
+        <div className="movie-overlay">
+          <button className="favorite-btn" onClick={onFavoriteClick}>
+            🤍
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
